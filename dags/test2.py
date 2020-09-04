048474ce977b7096a99e3a5814cad26446f0eac0
mikaela.pisani <mikaela.pisani@rootstrap.com>
2020-09-03 22:18:25 -0300

    import s3 transformer

--- dags/test2.py
+++ dags/test2.py
@@ -1,9 +1,5 @@
 from airflow import DAG
-from airflow.operators import DummyOperator, PythonOperator
-
-
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.operators import S3FileTransformOperator
 from datetime import datetime, timedelta
 
 
@@ -30,27 +26,4 @@ transformer = S3FileTransformOperator(
     transform_script='scripts/clean_medical_records.py'
 )
 
-
-
-# t1, t2 and t3 are examples of tasks created by instantiating operators
-t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
-
-t2 = BashOperator(task_id="sleep", bash_command="sleep 5", retries=3, dag=dag)
-
-templated_command = """
-    {% for i in range(5) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-        echo "{{ params.my_param }}"
-    {% endfor %}
-"""
-
-t3 = BashOperator(
-    task_id="templated",
-    bash_command=templated_command,
-    params={"my_param": "Parameter I passed in"},
-    dag=dag,
-)
-
-t2.set_upstream(t1)
-t3.set_upstream(t1)
+transformer
