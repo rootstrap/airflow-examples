a7bf719b3ca82f78f147b509bcd11ee1ab587687
mikaela.pisani <mikaela.pisani@rootstrap.com>
2020-09-07 14:18:37 -0300

    edit default arfs

--- dags/test2.py
+++ dags/test2.py
@@ -6,14 +6,14 @@ from datetime import datetime, timedelta
 
 
 default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime(2015, 6, 1),
-    "email": ["mikaela.pisani@rootstrap.com"],
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5)
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 11, 1),
+    'email': ['something@here.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
 }
 
 dag = DAG("s3_example", default_args=default_args, schedule_interval=timedelta(1))
