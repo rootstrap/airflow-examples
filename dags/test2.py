c72be774af0ffe946e71b79502dd64fbeac77d53
mikaela.pisani <mikaela.pisani@rootstrap.com>
2020-09-07 14:31:09 -0300

    add with dag

--- dags/test2.py
+++ dags/test2.py
@@ -16,25 +16,24 @@ default_args = {
     "retry_delay": timedelta(minutes=5)
 }
 
-dag = DAG("s3_example", default_args=default_args, schedule_interval=timedelta(1))
+with DAG("s3_example", default_args=default_args, schedule_interval=timedelta(1)) as dag:
+
+    t1 = BashOperator(
+        task_id='bash_test',
+        bash_command='echo "hello, it should work" > s3_conn_test.txt'
+    )
 
 
-t1 = BashOperator(
-    task_id='bash_test',
-    bash_command='echo "hello, it should work" > s3_conn_test.txt',
-    dag=dag)
+    transformer = S3FileTransformOperator(
+        task_id='etl_medical_records',
+        description='cleans medical etl_medical_records',
+        source_s3_key='s3://rs-champz-test/original_data/*',
+        dest_s3_key='s3://rs-champz-test/cleaned_data/',
+        replace=False,
+        transform_script='scripts/clean_medical_records.py',
+        source_aws_conn_id='s3_connection',
+        dest_aws_conn_id='s3_connection'
+    )
 
-transformer = S3FileTransformOperator(
-    task_id='etl_medical_records',
-    description='cleans medical etl_medical_records',
-    source_s3_key='s3://rs-champz-test/original_data/*',
-    dest_s3_key='s3://rs-champz-test/cleaned_data/',
-    replace=False,
-    transform_script='scripts/clean_medical_records.py',
-    source_aws_conn_id='s3_connection',
-    dest_aws_conn_id='s3_connection',
-    dag=dag
-)
-
-t1.set_upstream(transformer)
+    t1.set_upstream(transformer)
 
