31dcd63d0baa7bff57da4a5dd195b046c63b8a85
mikaela.pisani <mikaela.pisani@rootstrap.com>
2020-09-15 12:33:28 -0300

    add test3 - patient table

--- dags/test4.py
+++ dags/test4.py
@@ -9,22 +9,35 @@ class XComEnabledAWSAthenaOperator(AWSAthenaOperator):
         # just so that this gets `xcom_push`(ed)
         return self.query_execution_id
 
-with DAG(dag_id='athena_query_and_move',
-         schedule_interval=None,
-         start_date=datetime(2019, 6, 7)) as dag:
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2020, 9, 7),
+    "email": ["mikaela.pisani@rootstrap.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5)
+}
+
+with DAG("query_s3_athena", default_args=default_args, schedule_interval= '@once') as dag:
 
     run_query = XComEnabledAWSAthenaOperator(
         task_id='run_query',
-        query='SELECT * FROM  patient',
-        output_location='s3://rs-champz-test/result-test/',
-        database='prototype'
+        database='prototype',
+        query='SELECT * FROM patient',
+        output_location='s3://rs-champz-test/result-test',
+        aws_conn_id='s3_connection'
     )
     
     move_results = S3FileTransformOperator(
         task_id='move_results',
         source_s3_key='s3://mybucket/mypath/{{ task_instance.xcom_pull(task_ids="run_query") }}.csv',
         dest_s3_key='s3://rs-champz-test/parquet-test/{{ task_instance.xcom_pull(task_ids="run_query") }}.parquet',
-        transform_script='/opt/airflow/dags/scripts/parquet_test.py'
+        transform_script='/opt/airflow/dags/scripts/parquet_test.py',
+        source_aws_conn_id='s3_connection',
+        dest_aws_conn_id='s3_connection'
+        replace=False
     )
     
 move_results.set_upstream(run_query)
