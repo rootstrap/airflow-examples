ad5fc91b8af382bffd58e8bf82198d7d52c916e0
mikaela.pisani <mikaela.pisani@rootstrap.com>
2020-11-09 12:25:59 -0300

    update apache livy

--- dags/livy_example2.py
+++ dags/livy_example2.py
@@ -6,12 +6,13 @@ Example taken from https://github.com/apache/airflow/blob/master/airflow/provide
 """
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.operator.python import PythonOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.operators.http_operator import SimpleHttpOperator
 from airflow.sensors.http_sensor import HttpSensor 
 
 from datetime import datetime, timedelta
-
+import json 
+from enum import Enum
 
 default_args = {
     "owner": "airflow",
@@ -24,68 +25,86 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
-dag = DAG("livy-test2", default_args=default_args,schedule_interval= '@once')
+dag = DAG("livy-test2", default_args=default_args, schedule_interval= '@once')
 
 t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
 
 """
 Statement State
-Value   Description
-waiting Statement is enqueued but execution hasn't started
-running Statement is currently running
-available   Statement has a response ready
-error   Statement failed
-cancelling  Statement is being cancelling
-cancelled   Statement is cancelled
+    Value   Description
+    waiting Statement is enqueued but execution hasn't started
+    running Statement is currently running
+    available   Statement has a response ready
+    error   Statement failed
+    cancelling  Statement is being cancelling
+    cancelled   Statement is cancelled
+    success
+    dead
 """
+
+class State(Enum):
+    waiting = 'waiting'
+    running = 'running'
+    available = 'available'
+    error = 'available'
+    cancelling = 'cancelling'
+    cancelled = 'cancelled'
+    dead = 'dead'
+    success = 'success'
+
 def check_state(response):
     value = response.json()['state']
     print('state=:' + value)
-    return (value == 'available' or value == 'error' or value == 'cancelled')
+    state = str(State(value))
+    return (state == 'success' or state=='error' or state=='cancelling' or state =='cancelled')
 
-
+def get_id(**context):
+    response = context['ti'].xcom_pull(task_ids='spark-test-livy')
+    print('Respose:', response)
+    response = json.loads(response)
+    return response['id']
 
 spark_task  = SimpleHttpOperator(
     task_id='spark-test-livy',
     method='POST',
     endpoint='/batches',
-    data={
-        "name": "SparkPi-01",
+    data=json.dumps({
+        "name": "SparkPi-06", #TODO: generate id 
         "className": "org.apache.spark.examples.SparkPi",
         "numExecutors": 2,
         "file": "local:///opt/spark/examples/src/main/python/pi.py",
         "args": ["10"],
         "conf": {
             "spark.kubernetes.container.image": "mikaelapisani/spark-py:1.0",
+            "spark.kubernetes.driver.pod.name" : "spark-pi-driver",
             "spark.kubernetes.authenticate.driver.serviceAccountName": "spark"
         }
-      },
+      }),
     headers={'Content-Type': 'application/json'},
     xcom_push=True,
-    response_check=check_state,
     http_conn_id='livy_conn_id',
     dag=dag
 )
 
-def print_response(response):
-    print("######################################")
-    print(response)
-    print("######################################")
 
 
-print_response = task_archive_s3_file = PythonOperator(
-    task_id='archive_s3_file',
-    dag=dag,
-    python_callable=obj.print_response,
+get_id  = PythonOperator(
+    task_id='get_id',
+    python_callable=get_id,
     provide_context=True,
-    dag)
+    xcom_push=True,
+    dag=dag
+    )
 
-"""spark_sensor = HttpSensor(
+spark_sensor = HttpSensor(
     task_id='spark-sensor-livy',
     method='GET',
-    endpoint='/batches',
+    endpoint="/batches/{{ti.xcom_pull(task_ids='get_id')}}/state",
     http_conn_id='livy_conn_id',
-    )"""
+    dag=dag,
+    response_check=check_state
+    )
 
 spark_task.set_upstream(t1)
-print_response.set_upstream(spark_task)
+get_id.set_upstream(spark_task)
+spark_sensor.set_upstream(get_id)
